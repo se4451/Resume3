21ac04e54a42c17168e6f058fe6e2047040f3929
saeedebrahimi <ebrahimi.s1382@gmail.com>
2025-08-16 20:42:50 +0330

    Resume3

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import CyberpunkCity from "@/components/cyberpunk-city"
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full overflow-hidden bg-black">
+      <CyberpunkCity />
+    </main>
+  )
+}
