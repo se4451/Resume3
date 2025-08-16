714f5d5f42909913645354cd5d5649b8b5dd3d00
se4451 <ebrahimi.s1382@gmail.com>
2025-08-16 23:21:02 +0330

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import CyberpunkCity from "@/components/cyberpunk-city"
-
-export default function Home() {
-  return (
-    <main className="h-screen w-full overflow-hidden bg-black">
-      <CyberpunkCity />
-    </main>
-  )
-}
